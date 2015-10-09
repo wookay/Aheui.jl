31726542ea0b84be4490d918cb1fc9a2b1703da6
wookyoung <wookay.noh@gmail.com>
2015-10-10 02:45:41 +0900

    init

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,11 @@
+push!(LOAD_PATH, "src")
+push!(LOAD_PATH, "../src")
 using Aheui
+
 using Base.Test
 
-# write your own tests here
-@test 1 == 1
+@test "" == 아희("")
+@test "" == 아희("아희")
+@test "" == 아희("밯망희")
+@test "45" == 아희("발밞따망희")
+@test "5" == 아희("반받다망희")
